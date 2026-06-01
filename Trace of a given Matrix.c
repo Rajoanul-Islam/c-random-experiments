c1892dc7cebb034d4a1a1045b9a3c713e4716951
Rajoanul Islam <rajoanulislamzarif@gmail.com>
2026-06-01 11:28:44 +0600

    Update and rename Trace of a Matrix.c to Trace of a given Matrix.c

---
+++ Trace of a given Matrix.c
@@ -0,0 +1,29 @@
+//Finding the trace of a 3*3 matrix
+#include<stdio.h>
+int main(){
+
+int matrix[3][3]={
+
+    {5,8,7},
+    {6,8,9},
+    {2,4,9},
+
+};
+    int i,j, sum;
+    printf("The elements of the 3*3 matrix are: \n");
+for(i=0; i<3; i++){
+    for(j=0; j<3; j++){
+
+    printf("%d\t",matrix[i][j]); //prints a column to the same line with whitespace
+    }
+    printf("\n");//prints a row in a new line
+}
+
+//printing the elements required for finding the trace
+printf("Given,\nMatrix[1][1]=%d,    ", matrix[0][0]);
+printf("Matrix[2][2]=%d    ", matrix[1][1]);
+printf("& Matrix[3][3]=%d   \n", matrix[2][2]);
+printf("So, the trace of the matrix is=%d", sum= matrix[0][0] + matrix[1][1] + matrix[2][2]); //Printing the value of trace
+
+return 0;
+}
